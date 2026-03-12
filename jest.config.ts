2dcb2c2c3e03931c0c8ee0ac74bae0c193bfa203
SeungWoo <kangsw1025@naver.com>
2026-03-13 03:35:26 +0900

    chore: jest에서 e2e 폴더 제외

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testPathIgnorePatterns: ["/node_modules/", "/e2e/"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
   },
